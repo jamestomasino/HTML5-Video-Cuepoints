508314370c8e20f0061ce17811334bc98c1f1b8b
James Tomasino <git@jamestomasino.com>
2012-06-19 15:57:31 -0400

    removed manual script embeds. Namespace handles it now.

--- libs/Namespace.class.js
+++ libs/Namespace.class.js
@@ -17,20 +17,28 @@
 		return parent;
 	}
 
+	Namespace.baseURL = '';
+
 	Namespace.import = function ( scope, namespaceString ) {
 		var parts = namespaceString.split('.'),
-		parent = window,
+		parent = Namespace.global,
 		currentPart = '';
 
 		for(var i = 0, length = parts.length; i < length; i++) {
 			currentPart = parts[i];
 			if (currentPart != '*') {
 				// if it doesn't exist, just ignore
-				if ( typeof parent[currentPart] == 'undefined')
-					return;
+				if ( typeof parent[currentPart] == 'undefined') {
+					Namespace.load ( namespaceString );
+					if ( typeof parent[currentPart] == 'undefined') {
+						throw ('ERROR::[ Namespace does not exist: ' + namespaceString + ' ]' );
+						return;
+					}
+				}
 				parent = parent[currentPart];
 			} else if (i != length -1) {
 				// badly formatted. * is not last object: ignore
+				throw ('ERROR::[ Namespace badly formatted: ' + namespaceString + ' ]' );
 				return;
 			}
 		}
@@ -42,6 +50,7 @@
 					throw ('ERROR::[ Namespace collision: ' + namespaceString + ' ]' );
 				} else {
 					scope[id] = parent[id];
+					return;
 				}
 			}
 		} else {
@@ -49,9 +58,47 @@
 				throw ('ERROR::[ Namespace collision: ' + namespaceString + ' ]' );
 			} else {
 				scope[currentPart] = parent;
+				return;
 			}
 		}
 
+		throw ('ERROR::[ Nothing to process: ' + namespaceString + ' ]' );
+	}
+
+	Namespace.load = function ( namespaceString ) {
+		var xhrObj = Namespace.createXMLHTTPObject();
+		var scriptURL = namespaceString;
+		while (scriptURL.indexOf('.') != -1) {
+			scriptURL = scriptURL.replace('.', '/');
+		}
+		scriptURL = scriptURL + '.class.js';
+		xhrObj.open('GET', Namespace.baseURL + scriptURL, false);
+		xhrObj.send('');
+		var se = Namespace.global.document.createElement('script');
+		se.type = "text/javascript";
+		se.text = xhrObj.responseText;
+		Namespace.global.document.getElementsByTagName('head')[0].appendChild(se);
+	}
+
+	Namespace.XMLHttpFactories = [
+		function () {return new XMLHttpRequest()},
+		function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+		function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+		function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+	];
+
+	Namespace.createXMLHTTPObject = function () {
+		var xmlhttp = false;
+		for (var i = 0; i < Namespace.XMLHttpFactories.length; i++) {
+
+			try {
+				xmlhttp = Namespace.XMLHttpFactories[i]();
+			} catch (e) {
+				continue;
+			}
+			break;
+		}
+		return xmlhttp;
 	}
 
 	Namespace.global.Namespace = Namespace;
